70b63330eb9b9dce595385cabce2a5e92e990ee8
Igor Augusto <igoraugustobz@gmail.com>
2024-11-02 23:00:54 -0300

    :test_tube: test: testes unitários para as novas funcionalidades da classe Field.class

--- campo-minado-api/src/test/java/com/iaugusto/campo_minado/model/FieldTest.java
+++ campo-minado-api/src/test/java/com/iaugusto/campo_minado/model/FieldTest.java
@@ -114,12 +114,12 @@ class FieldTest {
     @Test
     @DisplayName("Não deve abrir os campos adjacentes que estão minados")
     void mustNotOpenAdjacentFieldsThatAreMined() {
-        Field neighbor11 = new Field(1,1);
-        Field neighbor12 = new Field(1,1);
+        Field neighbor11 = new Field(1, 1);
+        Field neighbor12 = new Field(1, 1);
 
         neighbor12.mine();
 
-        Field neighbor22 = new Field(2,2);
+        Field neighbor22 = new Field(2, 2);
         neighbor22.addNeighbors(neighbor11);
         neighbor22.addNeighbors(neighbor12);
 
@@ -127,5 +127,118 @@ class FieldTest {
         field.opened();
 
         assertTrue(neighbor22.isOpen() && neighbor11.isClosed());
+    }
+
+    @Test
+    @DisplayName("Deve revelar e proteger um campo com sucesso")
+    void mustSuccessfullyRevealAndProtectField() {
+        field.opened();
+        boolean result = field.objectiveAchieved();
+
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("Deve esperar uma mina nos vizinhos e retornar a contagem de minas na vizinhança com sucesso")
+    void mustWaitForMineInTheNeighborsAndReturnCountOfMinesInVicinitySuccessfully() {
+        Field fieldMined1 = new Field(3, 4);
+        Field fieldMined2 = new Field(4, 3);
+
+        fieldMined1.mine();
+
+        field.addNeighbors(fieldMined1);
+        field.addNeighbors(fieldMined2);
+
+        long amountMines = field.minesInTheNeighborhood();
+
+        assertEquals(1L, amountMines);
+        assertNotNull(amountMines);
+    }
+
+    @Test
+    @DisplayName("Deve esperar zero minas nos vizinhos com sucesso")
+    void mustExpectZeroMinesOnNeighborsWithSuccess() {
+        Field neighbor1 = new Field(3, 4);
+        Field neighbor2 = new Field(4, 3);
+
+        field.addNeighbors(neighbor1);
+        field.addNeighbors(neighbor2);
+
+        assertEquals(0, field.minesInTheNeighborhood(), "Esperado 0 minas nos vizinhos.");
+    }
+
+
+    @Test
+    @DisplayName("Deve reiniciar o jogo com sucesso")
+    void mustRestartTheGameSuccessfully() {
+        field.opened();
+        field.mine();
+        field.toogleTag();
+
+        field.restart();
+
+        assertFalse(field.isOpen());
+        assertFalse(field.isMarked());
+    }
+
+    @Test
+    @DisplayName("Deve retornar o símbolo correspondente ao campo marcado com sucesso")
+    void mustReturntheSymbolCorrespondingFieldMarkedSuccessfully() {
+        field.toogleTag();
+        String result = field.toString();
+
+        assertEquals("x", result);
+        assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("Deve retornar o símbolo correspondente ao campo aberto e minado com sucesso")
+    void mustReturntheSymbolCorrespondingFieldOpenedAndMinedSuccessfully() {
+        field.opened();
+        field.mine();
+
+        String result = field.toString();
+
+        assertEquals("*", result);
+        assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("Deve retornar o símbolo correspondente a quantidade com sucesso")
+    void mustReturntheSymbolCorrespondingFieldAmountSfuccessfully() {
+        Field fieldMined1 = new Field(3, 4);
+        Field fieldMined2 = new Field(4, 3);
+
+        fieldMined1.mine();
+
+        field.addNeighbors(fieldMined1);
+        field.addNeighbors(fieldMined2);
+
+        field.opened();
+
+        String result = field.toString();
+
+        assertEquals("1", result);
+        assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("Deve retornar um espaço vazio quando o campo estiver aberto")
+    void mustReturnEmptySpaceWhenFieldIsOpen() {
+        field.opened();
+
+        String result = field.toString();
+
+        assertEquals(" ", result);
+        assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("Deve retornar o símbolo de interrogação quando for a exceção")
+    void mustReturnQuestionMarkWhenItIsException() {
+        String result = field.toString();
+
+        assertEquals("?", result);
+        assertNotNull(result);
     }
 }
